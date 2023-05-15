655dc56fe19c304f923e97f190024cf1b77ceabb
Anandhu_P_A <anandhupa131@gmail.com>
2023-05-15 12:57:28 +0530

    navbar changed

--- app.js
+++ app.js
@@ -3,31 +3,28 @@ const mobile_menu = document.querySelector('.header .nav-menu .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-menu .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
 
-// document.addEventListener('scroll', () => {
-// 	var scroll_position = window.scrollY;
-// 	if (scroll_position > 250) {
-// 		header.style.backgroundColor = '#E6E6E6';
-// 	} else {
-// 		header.style.backgroundColor = 'transparent';
-// 	}
-// });
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#E6E6E6';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
 
 
-//navbar
- // slide-up script
-
-
-  $(".navbar .menu li a").click(function () {
-    // applying again smooth scroll on menu items click
-    $("html").css("scrollBehavior", "smooth");
-  });
-
-  // toggle menu/navbar script
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
-  });
 
 // let resumeBtn = document.querySelector("#resume");
 // resumeBtn.addEventListener("click",(e)=>{
