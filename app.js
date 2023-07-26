4a7f851a40930bfc3fad8192767a0be2d5ec6087
Anandhu_P_A <anandhupa131@gmail.com>
2023-07-26 21:13:54 +0530

    dyanamic navbar added

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const hamburger = document.querySelector('.header .nav-menu .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-menu .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-menu .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const header = document.querySelector('.navbar');
 
 
-// document.addEventListener('scroll', () => {
-// 	var scroll_position = window.scrollY;
-// 	if (scroll_position > 250) {
-// 		header.style.backgroundColor = '#E6E6E6';
-// 	} else {
-// 		header.style.backgroundColor = 'transparent';
-// 	}
-// });
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	
+   if (scroll_position > 3700) {
+		header.style.backgroundColor = '#000';
+	} 
+  else if (scroll_position > 3100) {
+		header.style.backgroundColor = '#fff';
+	} 
+  else if (scroll_position > 2600) {
+		header.style.backgroundColor = '#000';
+	} 
+  else if (scroll_position > 1100) {
+		header.style.backgroundColor = '#E6E6E6';
+	} 
+  else if (scroll_position > 500) {
+		header.style.backgroundColor = '#fff';
+	} 
+  else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
 
 
 //navbar
--- index.js
+++ index.js
@@ -1,12 +1,129 @@
 let projectContainer = document.getElementById("projectContainer");
 
+
 let data = [
     {
-        title : "project title",
+        title : "Codemate",
+        tag1  : "Group Project",
         description : "An appointment booking app for students to efficiently schedule and manage sessions with instructors.",
-        image : "./images/Role Based Access control.png",
+        image : "Role Based Access control.png",
         deployedLink : "https://tutor-track.vercel.app/",
         githubLink : "https://github.com/Anandhupa1/TutorTrack",
+        techStack : [{name : "HTML",image:"html.svg"},{name : "CSS",image:"css.svg"},{name:"Javascript",image:"javascript.svg"},
+                    {name:"Nodejs",image:"node.svg"},
+                    ]
 
-    }
+    },
+    {
+        title : "Codemate",
+        tag1  : "Individual Project",
+        tag1Color:"#000",
+        description : "An appointment booking app for students to efficiently schedule and manage sessions with instructors.",
+        image : "Role Based Access control.png",
+        deployedLink : "https://tutor-track.vercel.app",
+        githubLink : "https://github.com/Anandhupa1/TutorTrack",
+        techStack : [{name : "HTML",image:"html.svg"},{name : "CSS",image:"css.svg"},{name:"Javascript",image:"javascript.svg"},
+                    {name:"Nodejs",image:"node.svg"},
+                    ]
+
+    },
+    {
+        title : "Codemate",
+        tag1  : "Individual Project",
+        tag1Color:"#000",
+        description : "An appointment booking app for students to efficiently schedule and manage sessions with instructors.",
+        image : "Role Based Access control.png",
+        deployedLink : "https://tutor-track.vercel.app",
+        githubLink : "https://github.com/Anandhupa1/TutorTrack",
+        techStack : [{name : "HTML",image:"html.svg"},{name : "CSS",image:"css.svg"},{name:"Javascript",image:"javascript.svg"},
+                    {name:"Nodejs",image:"node.svg"},
+                    ]
+
+    },
+    {
+        title : "Codemate",
+        tag1  : "Individual Project",
+        tag1Color:"#000",
+        description : "An appointment booking app for students to efficiently schedule and manage sessions with instructors.",
+        image : "Role Based Access control.png",
+        deployedLink : "https://tutor-track.vercel.app",
+        githubLink : "https://github.com/Anandhupa1/TutorTrack",
+        techStack : [{name : "HTML",image:"html.svg"},{name : "CSS",image:"css.svg"},{name:"Javascript",image:"javascript.svg"},
+                    {name:"Nodejs",image:"node.svg"},
+                    ]
+
+    },
+    {
+        title : "Codemate",
+        tag1  : "Individual Project",
+        tag1Color:"#000",
+        description : "An appointment booking app for students to efficiently schedule and manage sessions with instructors.",
+        image : "Role Based Access control.png",
+        deployedLink : "https://tutor-track.vercel.app",
+        githubLink : "https://github.com/Anandhupa1/TutorTrack",
+        techStack : [{name : "HTML",image:"html.svg"},{name : "CSS",image:"css.svg"},{name:"Javascript",image:"javascript.svg"},
+                    {name:"Nodejs",image:"node.svg"},
+                    ]
+
+    },
+
 ]
+
+
+displayProjects(data)
+
+function displayProjects(arr){
+projectContainer.innerHTML=null;
+    arr.forEach((item)=>{
+    let div = document.createElement("div");
+    div.classList.add("project-card");
+    div.innerHTML=`
+    <div  class="box">
+
+    <img class="projectImage" src="./images/${item.image}" alt=${item.title} width="100%" height="120%">
+    <div class="project-title text"  >
+     
+      <span>${item.title}</span>
+      <span style="color:${item.tag1Color};"  class="badge1">${item.tag1||""}</span>
+
+    </div>
+    <p class="project-description">
+     ${item.description}
+    </p>
+    <div class="project-tech-stack">
+     
+    
+  
+    ${
+        item.techStack.map((elem)=>{
+        return techBadge(elem)
+        }).join("")
+    }
+
+    </div>
+    
+    <div class="sourceCodeBtn">
+        <button onclick="window.open('${item.deployedLink}','_blank')" class="viewBtn project-deployed-link">
+          <i id="viewIcon" class="fa-solid fa-eye"></i>  view 
+        </button>
+        <button onclick="window.open('${item.githubLink}','_blank')" class="viewBtn project-github-link">
+          <i id="viewIcon" class="fa-brands fa-github"></i>  Github
+        </button>
+        <button style="color:${item.tag1Color};" class="projectDuration">
+         <i style="color:${item.tag1Color};" id="durationIcon" class="fa-regular fa-clock"></i>  5 Days
+        </button>
+    </div>
+</div>
+    `
+    projectContainer.append(div);
+    })
+}
+
+
+function techBadge(obj){
+    return (`  <div class="badge">
+    <img src="./images/${obj.image}" alt="HTML Logo">
+    <span class="badge-name">${obj.name}</span>
+  </div>`
+  )
+}
